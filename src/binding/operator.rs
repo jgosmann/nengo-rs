b2bdae109e43994cabbc19718885ce39a243aa9f
Jan Gosmann <jan@hyper-world.de>
2020-07-18 18:42:39 +0200

    Add unit tests for binding::operator

--- src/binding/operator.rs
+++ src/binding/operator.rs
@@ -129,3 +129,82 @@ impl PyCopy {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::binding::signal::{PySignalArrayF64, PySignalF64, PySignalU64};
+    use crate::venv::activate_venv;
+    use pyo3::{types::IntoPyDict, wrap_pymodule, ToPyObject};
+
+    #[pymodule]
+    fn operator(_py: Python, m: &PyModule) -> PyResult<()> {
+        m.add_class::<PyCopy>()?;
+        m.add_class::<PyElementwiseInc>()?;
+        m.add_class::<PyReset>()?;
+        m.add_class::<PyTimeUpdate>()?;
+
+        m.add_class::<PySignalF64>()?;
+        m.add_class::<PySignalU64>()?;
+        m.add_class::<PySignalArrayF64>()?;
+
+        Ok(())
+    }
+
+    const DUMMY_SIGNAL_CONSTRUCTOR: &str =
+        "o.SignalArrayF64(nengo.builder.signal.Signal(np.zeros(1)))";
+
+    fn can_instantiate(expr: &str) -> PyResult<()> {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        activate_venv(py);
+        let nengo = PyModule::import(py, "nengo")?;
+        let numpy = PyModule::import(py, "numpy")?;
+        let operator_module = wrap_pymodule!(operator)(py);
+        let locals = [
+            ("nengo", nengo.to_object(py)),
+            ("np", numpy.to_object(py)),
+            ("o", operator_module),
+        ]
+        .into_py_dict(py);
+
+        let py_operator = py.eval(expr, None, Some(locals))?;
+        py_operator.extract::<&PyCell<PyOperator>>()?;
+        Ok(())
+    }
+
+    #[test]
+    fn can_instantiate_copy() {
+        can_instantiate(&format!(
+            "o.Copy({}, {}, [0])",
+            DUMMY_SIGNAL_CONSTRUCTOR, DUMMY_SIGNAL_CONSTRUCTOR
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn can_instantiate_elementwise_inc() {
+        can_instantiate(&format!(
+            "o.ElementwiseInc({}, {}, {}, [0])",
+            DUMMY_SIGNAL_CONSTRUCTOR, DUMMY_SIGNAL_CONSTRUCTOR, DUMMY_SIGNAL_CONSTRUCTOR
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn can_instantiate_reset() {
+        can_instantiate(&format!(
+            "o.Reset(np.zeros(1), {}, [0])",
+            DUMMY_SIGNAL_CONSTRUCTOR
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn can_instantiate_time_update() {
+        can_instantiate(
+            "o.TimeUpdate(0.001, o.SignalU64('step', 0), o.SignalF64('time', 0.), [0])",
+        )
+        .unwrap();
+    }
+}
