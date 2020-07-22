7811454613f9d1f5337a6b13b47d94aa93b27be7
Jan Gosmann <jan@hyper-world.de>
2020-07-22 20:24:11 +0200

    Make code DRYer by using a macro to define binding

--- src/binding/operator.rs
+++ src/binding/operator.rs
@@ -1,6 +1,7 @@
 use crate::binding::signal::PySignal;
 use crate::binding::Wrapper;
-use crate::operator::{CopyOp, ElementwiseInc, OperatorNode, Reset, TimeUpdate};
+use crate::operator;
+use crate::operator::OperatorNode;
 use crate::signal::ArraySignal;
 use ndarray::ArrayD;
 use numpy::PyArrayDyn;
@@ -19,116 +20,76 @@ impl Wrapper<Arc<OperatorNode>> for PyOperator {
     }
 }
 
+macro_rules! bind_op {
+    (
+        $name:ident: $op_type:ident $(< $($op_typearg:ty),+ >)?,
+        ($(arg $aname:ident : $atype:ty,)*
+        $(sig $sig:ident),*), {$($fname:ident : $expr:expr),*}
+    ) => {
+        #[pymethods]
+        impl $name {
+            #[new]
+            fn new(
+                $($aname: $atype,)*
+                $(
+                    $sig : &PySignal,
+                )*
+                dependencies: Vec<usize>,
+            ) -> PyResult<(Self, PyOperator)> {
+                Ok((
+                    Self {},
+                    PyOperator {
+                        node: Arc::new(OperatorNode {
+                            operator: Box::new(operator::$op_type$(::<$($op_typearg,)*>)? {
+                                $(
+                                    $sig : $sig.extract_signal("$sig")?,
+                                )*
+                                $($fname: $expr,)*
+                            }),
+                            dependencies,
+                        }),
+                    },
+                ))
+            }
+        }
+    };
+}
+
 #[pyclass(extends=PyOperator, name=Reset)]
 pub struct PyReset {}
 
-#[pymethods]
-impl PyReset {
-    #[new]
-    fn new(
-        value: &PyAny,
-        target: &PySignal,
-        dependencies: Vec<usize>,
-    ) -> PyResult<(Self, PyOperator)> {
-        let value: &PyArrayDyn<f64> = value.extract()?;
-        let value = value.to_owned_array();
-        Ok((
-            Self {},
-            PyOperator {
-                node: Arc::new(OperatorNode {
-                    operator: Box::new(Reset::<ArrayD<f64>, ArraySignal<f64>> {
-                        value,
-                        target: target.extract_signal("target")?,
-                    }),
-                    dependencies,
-                }),
-            },
-        ))
-    }
-}
+bind_op!(
+    PyReset: Reset<ArrayD<f64>, ArraySignal<f64>>,
+    (arg value: &PyAny, sig target),
+    {value: value.extract::<&PyArrayDyn<f64>>()?.to_owned_array()}
+);
 
 #[pyclass(extends=PyOperator, name=TimeUpdate)]
 pub struct PyTimeUpdate {}
 
-#[pymethods]
-impl PyTimeUpdate {
-    #[new]
-    fn new(
-        dt: f64,
-        step_target: &PySignal,
-        time_target: &PySignal,
-        dependencies: Vec<usize>,
-    ) -> PyResult<(Self, PyOperator)> {
-        Ok((
-            Self {},
-            PyOperator {
-                node: Arc::new(OperatorNode {
-                    operator: Box::new(TimeUpdate::<f64, u64> {
-                        dt,
-                        step_target: step_target.extract_signal("step_target")?,
-                        time_target: time_target.extract_signal("time_target")?,
-                    }),
-                    dependencies,
-                }),
-            },
-        ))
-    }
-}
+bind_op!(
+    PyTimeUpdate: TimeUpdate<f64, u64>,
+    (arg dt: f64, sig step_target, sig time_target),
+    { dt: dt }
+);
 
 #[pyclass(extends=PyOperator, name=ElementwiseInc)]
 pub struct PyElementwiseInc {}
 
-#[pymethods]
-impl PyElementwiseInc {
-    #[new]
-    fn new(
-        target: &PySignal,
-        left: &PySignal,
-        right: &PySignal,
-        dependencies: Vec<usize>,
-    ) -> PyResult<(Self, PyOperator)> {
-        Ok((
-            Self {},
-            PyOperator {
-                node: Arc::new(OperatorNode {
-                    operator: Box::new(ElementwiseInc::<f64> {
-                        target: target.extract_signal("target")?,
-                        left: left.extract_signal("left")?,
-                        right: right.extract_signal("right")?,
-                    }),
-                    dependencies,
-                }),
-            },
-        ))
-    }
-}
+bind_op!(
+    PyElementwiseInc: ElementwiseInc<f64>,
+    (sig target, sig left, sig right),
+    {}
+);
 
 #[pyclass(extends=PyOperator, name=Copy)]
 pub struct PyCopy {}
 
-#[pymethods]
-impl PyCopy {
-    #[new]
-    fn new(
-        src: &PySignal,
-        dst: &PySignal,
-        dependencies: Vec<usize>,
-    ) -> PyResult<(Self, PyOperator)> {
-        Ok((
-            Self {},
-            PyOperator {
-                node: Arc::new(OperatorNode {
-                    operator: Box::new(CopyOp::<ArrayD<f64>, ArraySignal<f64>> {
-                        src: src.extract_signal("src")?,
-                        dst: dst.extract_signal("dst")?,
-                        data_type: PhantomData,
-                    }),
-                    dependencies,
-                }),
-            },
-        ))
-    }
-}
+bind_op!(
+    PyCopy: CopyOp<ArrayD<f64>, ArraySignal<f64>>,
+    (sig src, sig dst),
+    { data_type: PhantomData }
+);
 
 #[cfg(test)]
 mod tests {
